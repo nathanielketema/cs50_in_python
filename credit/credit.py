2f25f9e0b9b05c546a72e5d7d2aa1d94fe727b7c
Nathaniel Ketema <nathanielketema@gmail.com>
2025-08-11 15:23:49 -0500

    feat: add an INVALID state and a few more test cases

--- credit/credit.py
+++ credit/credit.py
@@ -7,21 +7,25 @@ class CreditCard(Enum):
     AMEX = 1
     MASTERCARD = 2
     VISA = 3
+    INVALID = 4
 
 
-def which_credit_card(credit_number) -> CreditCard:
+def which_credit_card(credit_number: int) -> CreditCard:
     pass
 
 
 def main():
     """
-        Todo:
-        - Take user input 
-        - Validate user input
-        - Validate if it's a valid credit card
-        - Identify which company it belongs to
+    Todo:
+    - Take user input
+    - Validate user input
+    - Validate if it's a valid credit card
+    - Identify which company it belongs to
     """
-    pass
+    credit_number: int = int(input("Number: "))
+
+    result: CreditCard = which_credit_card(credit_number)
+    print(result.name)
 
 
 class TestCredit(unittest.TestCase):
@@ -29,6 +33,8 @@ class TestCredit(unittest.TestCase):
         self.assertEqual(which_credit_card(78282246310005), CreditCard.AMEX)
         self.assertEqual(which_credit_card(71449635398431), CreditCard.AMEX)
         self.assertEqual(which_credit_card(78734493671000), CreditCard.AMEX)
+        self.assertEqual(which_credit_card(378282246310005), CreditCard.AMEX)
+        self.assertEqual(which_credit_card(371449635398431), CreditCard.AMEX)
 
     def test_MASTERCARD(self):
         self.assertEqual(which_credit_card(2221000000000009), CreditCard.MASTERCARD)
@@ -36,10 +42,14 @@ class TestCredit(unittest.TestCase):
         self.assertEqual(which_credit_card(2223016768739313), CreditCard.MASTERCARD)
         self.assertEqual(which_credit_card(5555555555554444), CreditCard.MASTERCARD)
         self.assertEqual(which_credit_card(5105105105105100), CreditCard.MASTERCARD)
+        self.assertEqual(which_credit_card(5555555555554444), CreditCard.MASTERCARD)
 
     def test_VISA(self):
         self.assertEqual(which_credit_card(4111111111111111), CreditCard.VISA)
         self.assertEqual(which_credit_card(4012888888881881), CreditCard.VISA)
+
+    def test_INVALID(self):
+        self.assertEqual(which_credit_card(1234567890), CreditCard.INVALID)
 
 
 if __name__ == "__main__":
